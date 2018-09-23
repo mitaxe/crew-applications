0e3ae56a1a91f14e7edc43bbb748f67f7a89174a
BogdanNosovytskyy <b.nosovytskyy@gmail.com>
2018-09-23 23:32:10 +0300

    searching substrings

--- src/utils/boardUtils.js
+++ src/utils/boardUtils.js
@@ -1,8 +1,5 @@
-export const filterByCityAndName = (users, filters) => {
-  const { name, city } = filters
-  return users.filter(user => {
-    const nameFilter = name !== '' ? user.name.toLowerCase() === name.toLowerCase().trim() : user
-    const cityFilter = city !== '' ? user.city.toLowerCase() === city.toLowerCase().trim() : user
-    return nameFilter && cityFilter
+export const filterByCityAndName = (users, filters) =>
+  users.filter(user => {
+    const filtered = Object.keys(filters).map(key => user[key].indexOf(filters[key]) !== -1)
+    return filtered.every(item => item)
   })
-}
